2b967c2a1b5c56bbcd6de88d130c34ebb8093877
zeal1ng <egoraros75@gmail.com>
2026-05-25 08:58:22 +0300

    penis

--- obj/Debug/net10.0/ToDo Manager.AssemblyInfo.cs
+++ obj/Debug/net10.0/ToDo Manager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDo Manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e78e71b1dd693c8ea8b4131886d22b4a5ba45f19")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDo Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDo Manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
